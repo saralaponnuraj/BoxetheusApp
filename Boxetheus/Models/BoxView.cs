24e49f6f8a38e6ac56e5e910392b98122794492b
Sarala Ponnuraj <W0850015@myscc.ca>
2024-02-29 12:05:16 -0500

    Implemented controller methods and views, passed Data to Views

--- Boxetheus/Models/BoxView.cs
+++ Boxetheus/Models/BoxView.cs
@@ -7,25 +7,29 @@ namespace Boxetheus.Models
     public class BoxView
     {
         public int Id { get; set; }
-       
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Brand { get; set; }
-       
+        [Display(Name = "Order Date")]
         [DataType(DataType.Date)]
 
         public DateTime OrderDate { get; set; }
-        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(20)]
+        [Required]
         public string? Category { get; set; }
 
-       
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(20)]
+        [Required]
         public string? Design { get; set; }
         public string? Shape { get; set; }
         public string? Size { get; set; }
         public int Quantity { get; set; }
-       
+        [Range(1, 500)]
         [DataType(DataType.Currency)]
-       
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
-     
+        
     }
-
 }
